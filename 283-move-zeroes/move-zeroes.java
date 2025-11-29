41133e23b1e8d29aefa76e49aa325125514365d7
Samyog Ghimire <samyog.ghimire25@gmail.com>
2025-11-29 11:19:13 +0530

    Time: 2 ms (83.98%) | Memory: 47.5 MB (38.35%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       
+        int c = 0;
+        int n = nums.length;
+         int[] temp = new int[n];
+        for (int i=0; i<n; i++){
+            if (nums[i]!=0){
+                temp[c]=nums[i];
+                c++;
+            } 
+        }
+       while (temp.length<nums.length){
+        temp[c] = 0;
+        c++;
+       }
+       for (int i=0; i<temp.length;i++){
+        nums[i]=temp[i];
+       }
+    }
+}
