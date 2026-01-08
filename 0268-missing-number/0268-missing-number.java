f3af3391aa4a9999dd47b43bc058eb6fc98ed5ec
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-01-08 09:41:40 +0530

    Optimize missingNumber method for efficiency
    Refactored the missingNumber method to use sum calculation instead of sorting for improved efficiency.

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for (int i=0;i<nums.length;i++){
-            if(nums[i]!=i){
-                return i;
-            }
+        int n = nums.length;
+        int actualSum = n*(n+1)/2;
+        int sum = 0;
+
+        for (int i=0; i<n; i++){
+            sum += nums[i];
         }
-        return nums.length;
+        return actualSum - sum;
     }
 }
