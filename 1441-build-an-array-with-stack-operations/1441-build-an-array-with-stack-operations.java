beba5176c7454d441bd424ef9fabf1b6c9e3d2a6
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-01-20 05:41:45 +0530

    Time: 0 ms (100%), Space: 43.6 MB (98.79%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List <String> result = new ArrayList<>();
+        Stack <String> result = new Stack<>();
         int count = 1;
         int i = 0;
         while (i<target.length && count <= n){
-                result.add("Push");
+                result.push("Push");
                 if (count == target[i]){
                     i++;
                     count++;
                 } else if (count != target[i]){
-                    result.add("Pop");
+                    result.push("Pop");
                     count++;
                 }
         }
