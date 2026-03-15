fe7e958ac226f7f9d4266fe71bb9b65d4a11a361
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-03-15 23:58:53 +0530

    Time: 0 ms (100%), Space: 42 MB (84.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,7 +3,9 @@ class Solution {
         if (n == 0 || n == 1) {
             return 1;
         }
+
         int prev = 1, curr = 1;
+        
         for (int i = 2; i <= n; i++) {
             int temp = curr;
             curr = prev + curr;
