c3bcc4b64eacbf5ffbac3806a369399127d473e0
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-03-26 00:02:34 +0530

    Time: 1 ms (98.45%), Space: 46.7 MB (7.02%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -3,6 +3,7 @@ class Solution {
         int n= nums.length;
         int sum=0;
         int diff=0;
+        
         for(int i=0;i<n;i++){
             if(i%2==0){
                 sum=sum+nums[i];
