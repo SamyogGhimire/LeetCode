b13b4de62b641707a8856d086d3377a7b77035e5
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-05-11 23:58:56 +0530

    Time: 4 ms (55.82%), Space: 45.2 MB (59.96%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = prices.clone();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int idx = stack.pop();
+                ans[idx] = prices[idx] - prices[i];
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
