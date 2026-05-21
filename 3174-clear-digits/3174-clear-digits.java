b56fa3f74c4d853a77c1e15c2c565b6b55807a54
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-05-21 12:00:12 +0530

    Time: 3 ms (48.06%), Space: 43.7 MB (79.49%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch: s.toCharArray()){
+            if (Character.isDigit(ch)){
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c: stack){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
