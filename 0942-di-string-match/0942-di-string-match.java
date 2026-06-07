c46b87c71315b8d392a30c76e7d1fd9b28e736b1
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-07 07:59:21 +0530

    Time: 3 ms (38.67%), Space: 47.3 MB (43.53%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.java
+++ 0942-di-string-match/0942-di-string-match.java
@@ -6,7 +6,7 @@ class Solution {
         int countD = s.length();
         int countI = 0;
 
-        for (int i = 0; i < ch.length; i++){
+        for (int i = 0; i < s.length(); i++){
             if (ch[i] == 'D'){
                 ans[i] = countD;
                 countD --;
