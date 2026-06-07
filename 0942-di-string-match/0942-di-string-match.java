ffc4b97d26ef566788ecd03d6f334bdce957e39c
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-07 07:58:25 +0530

    Time: 2 ms (98.72%), Space: 47.5 MB (10.05%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int[] ans = new int[s.length() + 1];
+
+        char[] ch = s.toCharArray();
+        int countD = s.length();
+        int countI = 0;
+
+        for (int i = 0; i < ch.length; i++){
+            if (ch[i] == 'D'){
+                ans[i] = countD;
+                countD --;
+
+            } else if (ch[i] == 'I'){
+                ans[i] = countI;
+                countI++;
+            }
+        }
+        ans[s.length()] = countI; //countI=countD so anything can be written here
+        return ans;
+    }
+}
