16a0a8a97e2926b60266d16ed86b71ff1c94733c
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-17 21:07:58 +0530

    Time: 15 ms (62.73%), Space: 93.3 MB (67.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -9,3 +9,18 @@ class Solution {
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for (int num: nums){
+//             map.put(num, map.getOrDefault(num,0)+1);
+//         }
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+//             if (entry.getValue() > 1){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
