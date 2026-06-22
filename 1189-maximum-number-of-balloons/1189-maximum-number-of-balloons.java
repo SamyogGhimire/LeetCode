40895eeb75b689016b01c4e6cb787b89d99af181
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-22 06:05:19 +0530

    Time: 11 ms (35.13%), Space: 44.2 MB (14.36%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : text.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        return Math.min(
+                Math.min(map.getOrDefault('b', 0), map.getOrDefault('a', 0)),
+                Math.min(
+                        Math.min(map.getOrDefault('l', 0) / 2, map.getOrDefault('o', 0) / 2),
+                        Math.min(map.getOrDefault('n', 0), Integer.MAX_VALUE)
+                )
+        );
+    }
+}
