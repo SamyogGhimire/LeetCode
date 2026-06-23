40f834d71bc767b250bad02ca97076e41073be75
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-23 07:22:20 +0530

    Time: 7 ms (70.85%), Space: 46.9 MB (22.83%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -3,24 +3,27 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        int[][] ans = new int[m][n];
+        int[][] prefixSum = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                prefixSum[i][j] = mat[i-1][j-1] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+            }
+        }
 
+        int[][] ans = new int[m][n];
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                int sum = 0;
-
-                for (int r = i-k; r <= i+k; r++){
-                    for (int c = j-k; c <= j+k; c++){
-                        if (r >= 0 && r < m && c>=0 && c < n){
-                            sum += mat[r][c];
-                        }
-                       
-                    }
-                } 
-                ans[i][j] = sum;
+                int r1 = Math.max(0,i-k);
+                int c1 = Math.max(0,j-k);
+                int r2 = Math.min(m-1,i+k);
+                int c2 = Math.min(n-1,j+k);
+                r1++;
+                c1++;
+                r2++;
+                c2++;
+                ans[i][j] = prefixSum[r2][c2] - prefixSum[r1-1][c2] - prefixSum[r2][c1-1] + prefixSum[r1-1][c1-1];
             }
         }
         return ans;
-        
     }
 }
