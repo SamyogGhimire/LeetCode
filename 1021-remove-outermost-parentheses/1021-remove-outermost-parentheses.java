340831c53d8e5786ef53f405e180335675e162e3
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-06-26 06:59:56 +0530

    Time: 2 ms (99.73%), Space: 43.2 MB (96.43%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (char ch: s.toCharArray()){
+            if (ch ==')'){
+                count--;
+            }
+            if (count > 0){
+                sb.append(ch);
+            }
+            
+            if (ch == '('){
+                count++;
+            }
+        }
+        return sb.toString();
+    }
+}
