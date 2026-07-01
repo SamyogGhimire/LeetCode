27ee73b68dd925e0c24aa1e4272df9f76570436f
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-07-01 14:56:50 +0530

    Time: 0 ms (100%), Space: 44.3 MB (53.47%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int prefixSum = 0;
+        for (int i = 0; i < nums.length; i++){
+            prefixSum += nums[i];
+            nums[i] = prefixSum;
+        }
+        return nums;
+    }
+}
