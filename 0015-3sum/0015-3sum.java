389bdce1a29f0ba1290fa9104760b983667fd4bf
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-07-23 16:07:13 +0530

    Time: 35 ms (44.35%), Space: 59.2 MB (59.97%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
+        for (int i = 0; i < n-2; i++){
+            if (i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
+            while (j<k){
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j<k && nums[j] == nums[j+1]) j++;
+                    while (j<k && nums[k] == nums[k-1]) k--;
+                    j++;
                     k--;
-                } else if (total < 0) {
+                } else if (sum > 0){
+                    k--;
+                } else if (sum < 0){
                     j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
                 }
             }
         }
-        return res;        
+        return ans;
     }
 }
