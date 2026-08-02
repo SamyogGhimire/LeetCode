54d0b3f225ad0d9cb0d6edad0cfa32e998b75770
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-08-02 10:54:23 +0530

    Time: 0 ms (100%), Space: 46.7 MB (53.31%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
+        if (head == null) return false;
         ListNode slow = head;
+        ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
+        while (fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
-
-            if (fast == slow) {
+            if (fast == slow){
                 return true;
             }
         }
-
-        return false;        
+        return false;
     }
 }
