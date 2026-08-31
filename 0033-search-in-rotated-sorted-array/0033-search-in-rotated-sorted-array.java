90fa56aca4ca60a3075dcacff7951dd757fac43e
Samyog Ghimire <samyog.ghimire25@gmail.com>
2026-08-31 08:51:54 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,27 @@
-// class Solution {
-//     public int search(int[] nums, int target) {
-
-//         int left = 0;
-//         int right = nums.length - 1;
-
-        
-//         while (left < right){
-//             int mid = left + (right - left) / 2;
-//             if (nums[mid] == target) return mid;
-//             else if (nums[right] == target) return right;
-//             else if (nums[left] == target) return left;
-
-//             if (left > target && target < mid){
-//                 left = mid + 1;
-//             } else if (right < target && target > mid){
-//                 right = mid - 1;
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
-
 class Solution {
     public int search(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = nums.length -1;
 
-        for ( int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
-        }
+        while (left <= right){
+            int mid = left + (right - left) / 2;
 
-        
-        for (Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if (entry.getKey() == target){
-                return entry.getValue();
+            if (nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
